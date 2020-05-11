0c288138ecb0167e7a55852fc482f4c4bc73dd0c
jrcheeseman <jacob.cheeseman910@topper.wku.edu>
2020-05-11 18:20:01 +0200

    Initial commit

---
+++ R-Programming/myfiles/corr.R
@@ -0,0 +1,22 @@
+corr <- function(directory, threshold = 0) {
+  correlations <- vector("numeric")
+  for(i in 1:332) {
+    if (i < 10) {
+      paste(directory, "/00", i, ".csv", sep = "")
+    } else if (i < 100) {
+      filename <- paste(directory, "/0", i, ".csv", sep = "")
+    } else {
+      filename <- paste(directory, "/", i, ".csv", sep = "")
+    }
+    data <- read.csv(filename)
+    if (nrow(na.omit(data)) > threshold) {
+      correlations <- c(correlations, cor(x = data["sulfate"], y = data["nitrate"], use = "complete.obs"))
+    }
+  }
+  if (length(correlations) > 0) {
+    correlations
+  } else {
+    correlations <- vector("numeric")
+    correlations
+  }
+}
---
+++ R-Programming/myfiles/pollutantmean.R
@@ -0,0 +1,14 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  data <- data.frame()
+  for(i in id) {
+    if (i < 10) {
+      filename <- paste(directory, "/00", i, ".csv", sep = "")
+    } else if (i < 100) {
+      filename <- paste(directory, "/0", i, ".csv", sep = "")
+    } else {
+      filename <- paste(directory, "/", i, ".csv", sep = "")
+    }
+    data <- rbind(data, read.csv(filename))
+  }
+  mean(data[[pollutant]], na.rm = TRUE)
+}
