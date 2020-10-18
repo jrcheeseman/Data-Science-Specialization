8a6dd807680259b22f2e4dd7e8fe66fc7e93862f
jrcheeseman <jacob.cheeseman910@topper.wku.edu>
2020-10-18 20:26:00 +0200

    Add files via upload

---
+++ Getting-and-Cleaning-Data/run_analysis.R
@@ -0,0 +1,49 @@
+
+# Read subject column from training and test data
+test <- read.table("./test/subject_test.txt", col.names = c("subject"))
+train <- read.table("./train/subject_train.txt", col.names = c("subject"))
+
+# Replace activity numbers with labels and join
+test["activity"] <- join(read.table("./test/y_test.txt"), 
+                         read.table("./activity_labels.txt"), 
+                         by='V1') %>% select(V2)
+train["activity"] <- join(read.table("./train/y_train.txt"), 
+                          read.table("./activity_labels.txt"), 
+                          by='V1') %>% select(V2)
+
+# Read sensor data
+X_test <- read.table("./test/X_test.txt")
+X_train <- read.table("./train/X_train.txt")
+
+# Replace column headers with descriptive variable names
+features <- read.table("./features.txt")
+colnames(X_test) <- t(features[2])
+colnames(X_train) <- t(features[2])
+
+# Bind subject, activity, and sensor data
+test <- cbind(test, X_test)
+train <- cbind(train, X_train)
+
+# Add set indicator 
+test["set"] <- "TEST"
+train["set"] <- "TRAIN"
+
+# Move indicator column to first position
+test <- test[,c(ncol(test),1:(ncol(test)-1))]
+train <- train[,c(ncol(train),1:(ncol(train)-1))]
+
+# Bind rows from each subset to create one data set
+data <- rbind(test, train)
+
+# Subset the data to include only the mean and standard deviation for each measurement
+mean_columns <- data[grepl("mean", colnames(data), fixed=TRUE)]
+std_columns <- data[grepl("std", colnames(data), fixed=TRUE)]
+subset <- cbind(data["activity"], data["subject"], mean_columns, std_columns)
+
+# Create long skinny version of the variables to be summarized
+vars <- colnames(subset)
+long_skinny <- reshape2::melt(subset, id=c("activity","subject"), measure.vars=vars[3:81])
+
+# Create a tidy data set with the average of each variable for each activity and each subject
+groups <- group_by(long_skinny, variable, activity, subject)
+tidy_data <- dplyr::summarize(groups, average = mean(value))
